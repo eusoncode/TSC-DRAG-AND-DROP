356def13c478d838e2bd63926183471c9243c63f
eusoncode <git@github.com:eusoncode/Kata.git>
2024-04-03 01:09:51 -0600

    Access the values of the input on the form

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 //Access to the form template
 class ProjectInput {
+  //Define the variables
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement; //Access the input template html
@@ -11,6 +15,11 @@ class ProjectInput {
     const importedNode = document.importNode(this.templateElement.content, true); //Import the input template
     this.element = importedNode.firstElementChild as HTMLFormElement; // Access the form in the input template
     this.element.id = 'user-input'; // Style the form before rendering
+
+    this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement; // Access the values of the input on the form
+    this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement;
+    this.peopleInputElement= this.element.querySelector('#people')! as HTMLInputElement;
+
     this.attach(); // Render form on the div html
   }
 
