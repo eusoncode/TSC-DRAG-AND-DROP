aba1e3ea3ce12e76a6ed4d1b3a96de47f2b4edef
eusoncode <git@github.com:eusoncode/Kata.git>
2024-04-03 01:53:59 -0600

    Fetching User Input

--- src/app.ts
+++ src/app.ts
@@ -37,11 +37,33 @@ class ProjectInput {
     this.attach(); // Render form on the div html
   }
 
+  // Fetching User Input
+  private gatherUserInput(): [string, string, number] | void { 
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+    
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert('Invalid input, please try again!');
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople]
+    }
+  };
+
   // Action when button submit is clicked
   @autobind
   private submitHandler(e:Event) {
     e.preventDefault();
-    console.log(this.titleInputElement.value);    
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);  
+    }
   }
 
   // Listen for button submit event
