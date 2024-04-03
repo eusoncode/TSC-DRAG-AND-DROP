8be184cb65087ee21a0705262054719cd1b8e219
eusoncode <git@github.com:eusoncode/Kata.git>
2024-04-03 00:56:07 -0600

    DOM Selection and OOP Rending of form

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
+//Access to the form template
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
 
+  constructor() {
+    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement; //Access the input template html
+    this.hostElement = document.getElementById('app')! as HTMLDivElement; //Access the div html
+
+    const importedNode = document.importNode(this.templateElement.content, true); //Import the input template
+    this.element = importedNode.firstElementChild as HTMLFormElement; // Access the form in the input template
+    this.attach(); // Render form on the div html
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
