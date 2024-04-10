5cec7e6e69e83a0e0ba8ce98d89ed9ab08d84cf8
eusoncode <git@github.com:eusoncode/Kata.git>
2024-04-09 23:13:13 -0600

    Adding Entry & Output configuration

--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,7 +1,7 @@
-import { Draggable } from '../models/drag-drop.js';
-import { Project } from '../models/project.js';
-import { Component } from './base-component.js';
-import { autobind as Autobind } from '../decorators/autobind.js';
+import { Draggable } from '../models/drag-drop';
+import { Project } from '../models/project';
+import { Component } from './base-component';
+import { autobind as Autobind } from '../decorators/autobind';
 
 // ProjectItem class
 export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable{
